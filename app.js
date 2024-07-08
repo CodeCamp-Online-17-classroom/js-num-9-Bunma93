9e040ed2300bccc2162e6837e7f200c0d819d6cf
Bunma93 <bunma93@gmail.com>
2024-07-08 13:17:36 +0700

    Js-num9

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+function diagonalLength(width, height) {
+    return Math.sqrt(width * width + height * height);
+}
+
+let width = prompt("ความกว้างของสี่เหลี่ยม(ซม.)");
+let height = prompt("ความยาวของสี่เหลี่ยม(ซม.)");
+let diagonal = diagonalLength(width, height).toFixed(2);
+alert(`ความยาวเส้นแทยงมุมประมาณ ${diagonal} ซม.`);
